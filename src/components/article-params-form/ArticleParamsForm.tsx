7ff6e069063929af56a55c3bec3866646efc317c
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-10-16 00:20:48 +0300

    Добавил открытие окна с настройками страницы по клику на кнопку со стрелкой

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,21 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen(!isOpen);
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
