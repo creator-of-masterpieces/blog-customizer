b4a1c3088a48f6a25cb28e329893d2721a7a1aeb
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-10-16 06:52:37 +0300

    Добавил отображение выбранных опций в форме;

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -5,7 +5,7 @@ import { Select } from 'src/ui/select';
 import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import {
 	backgroundColors,
@@ -13,11 +13,77 @@ import {
 	fontColors,
 	fontFamilyOptions,
 	fontSizeOptions,
+	OptionType,
 } from 'src/constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 
 export const ArticleParamsForm = () => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [currentFont, setCurrentFont] = useState<OptionType | null>(
+		fontFamilyOptions[0]
+	);
+	const [currentFontColor, setCurrentFontColor] = useState<OptionType | null>(
+		fontColors[0]
+	);
+	const [backgroundColor, setBackgroundColor] = useState<OptionType | null>(
+		backgroundColors[0]
+	);
+	const [contentWidth, setContentWidth] = useState<OptionType | null>(
+		contentWidthArr[0]
+	);
+	const [fontSize, setFontSize] = useState<OptionType>(fontSizeOptions[0]);
+
+	const articleParamsFormRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		// Обработчик закрытия окна по Esc
+		function escapeHandler(evt: KeyboardEvent) {
+			evt.key === 'Escape' && setIsOpen(false);
+		}
+
+		// Обработчик закрытия окна по клику вне окна
+		function handleClickOutside(evt: MouseEvent) {
+			if (
+				articleParamsFormRef.current &&
+				evt.target instanceof Node &&
+				!articleParamsFormRef.current.contains(evt.target)
+			) {
+				setIsOpen(false);
+			}
+		}
+		document.addEventListener('keydown', escapeHandler);
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('keydown', escapeHandler);
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
+	// Обработчик клика по опции выбора шрифта
+	function fontOptionClickHandler(selectedOption: OptionType) {
+		setCurrentFont(selectedOption);
+	}
+
+	// Обработчик клика по опции выбора цвета текста
+	function colorOptionClickHandler(selectedOption: OptionType) {
+		setCurrentFontColor(selectedOption);
+	}
+
+	// Обработчик клика по опции выбора цвета фона
+	function backgroundColorOptionClickHandler(selectedOption: OptionType) {
+		setBackgroundColor(selectedOption);
+	}
+
+	// Обработчик клика по опции выбора ширины контента страницы
+	function contentWidthOptionClickHandler(selectedOption: OptionType) {
+		setContentWidth(selectedOption);
+	}
+
+	// Обработчик клика по опции выбора размера шрифта
+	function fontSizeOptionClickHandler(selectedOption: OptionType) {
+		setFontSize(selectedOption);
+	}
+
 	return (
 		<>
 			<ArrowButton
@@ -27,43 +93,49 @@ export const ArticleParamsForm = () => {
 				}}
 			/>
 			<aside
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={articleParamsFormRef}>
 				<form className={styles.form}>
 					<Text as='h2' size={31} weight={800} uppercase>
 						Задайте параметры
 					</Text>
 
 					<Select
-						selected={null}
+						selected={currentFont}
 						options={fontFamilyOptions}
 						placeholder={fontFamilyOptions[0].title}
-						title={'шрифт'}></Select>
+						title={'шрифт'}
+						onChange={fontOptionClickHandler}></Select>
 
 					<RadioGroup
 						name={'fontSizeGroup'}
+						selected={fontSize}
 						options={fontSizeOptions}
-						selected={fontSizeOptions[0]}
-						title={'размер шрифта'}></RadioGroup>
+						title={'размер шрифта'}
+						onChange={fontSizeOptionClickHandler}></RadioGroup>
 
 					<Select
-						selected={fontColors[0]}
+						selected={currentFontColor}
 						options={fontColors}
-						placeholder={fontFamilyOptions[0].title}
-						title={'цвет фона'}></Select>
+						placeholder={fontColors[0].title}
+						title={'цвет шрифта'}
+						onChange={colorOptionClickHandler}></Select>
 
 					<Separator></Separator>
 
 					<Select
-						selected={backgroundColors[0]}
+						selected={backgroundColor}
 						options={backgroundColors}
 						placeholder={backgroundColors[0].title}
-						title={'цвет фона'}></Select>
+						title={'цвет фона'}
+						onChange={backgroundColorOptionClickHandler}></Select>
 
 					<Select
-						selected={contentWidthArr[0]}
+						selected={contentWidth}
 						options={contentWidthArr}
 						placeholder={contentWidthArr[0].title}
-						title={'ширина контента'}></Select>
+						title={'ширина контента'}
+						onChange={contentWidthOptionClickHandler}></Select>
 
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
